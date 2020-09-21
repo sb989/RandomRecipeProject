9eb2b69a00c02821bc486a084fdcc894044fdccd
sb989 <sb989@njit.edu>
2020-09-21 02:15:23 +0000

    Added error message for when spoonacular credits are used up

--- proj1.py
+++ proj1.py
@@ -35,22 +35,39 @@ api = tweepy.API(auth)
 @app.route('/')
 def index():
     tweet = []
+    spoon = True
     while not tweet:
         url = "https://api.spoonacular.com/recipes/random?apiKey={}".format(spoonacular_key)
         response = requests.get(url)
         json_body = response.json()
+        
+        if json_body["status"] == 'failure':
+            spoon = False
+            break
         title = json_body["recipes"][0]["title"]
         title = title.strip('\"')
         print(title)
         tweet =api.search(q=title,lang="en",count=100)
-    size = len(tweet)
-    print(size)
-    tweet = tweet[random.randint(0,size-1)].text
-    return flask.render_template(
-        "index.html",
-        title = title ,
-        tweet = tweet
-        )
+    if spoon:
+        size = len(tweet)
+        print(size)
+        num = random.randint(0,size-1)
+        text = tweet[num].text
+        screen_name = tweet[num].user.screen_name
+        date = tweet[num].created_at
+        return flask.render_template(
+            "index.html",
+            title = title,
+            text = text,
+            screen_name = screen_name,
+            date = date
+            )
+    else:
+        msg = "Sorry. We've reached the max number of recipes for today. Please try again tomorrow."
+        return flask.render_template(
+            "index.html",
+            msg=msg
+            )
         
 app.run(
     port = int(os.getenv("PORT", 8080)),   
